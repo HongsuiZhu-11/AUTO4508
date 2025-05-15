5c9238120c0032785512545f66dca6f8931bb2da
shawnhg28 <23618264@student.uwa.edu.au>
2025-05-15 13:03:29 +0800

    update

--- src/imu_package/install/imu_package/share/imu_package/package.sh
+++ src/imu_package/install/imu_package/share/imu_package/package.sh
@@ -52,7 +52,7 @@ _colcon_prepend_unique_value() {
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_package_sh_COLCON_CURRENT_PREFIX="/home/team10/ws/AUTO4508/src/aria_pkg/install/aria_pkg"
+_colcon_package_sh_COLCON_CURRENT_PREFIX="/home/team10/ws/AUTO4508/src/imu_package/install/imu_package"
 if [ -z "$COLCON_CURRENT_PREFIX" ]; then
   if [ ! -d "$_colcon_package_sh_COLCON_CURRENT_PREFIX" ]; then
     echo "The build time path \"$_colcon_package_sh_COLCON_CURRENT_PREFIX\" doesn't exist. Either source a script for a different shell or set the environment variable \"COLCON_CURRENT_PREFIX\" explicitly." 1>&2
@@ -78,7 +78,8 @@ _colcon_package_sh_source_script() {
 }
 
 # source sh hooks
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/aria_pkg/local_setup.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/imu_package/hook/pythonpath.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/imu_package/hook/ament_prefix_path.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
